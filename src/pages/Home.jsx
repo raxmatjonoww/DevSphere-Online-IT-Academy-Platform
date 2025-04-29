3207fccd0399b3ef8c36a76e236ca9f87a905040
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-04-29 13:30:03 +0500

    DevSphere Online IT Academy Platform bugs fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './home.css'
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
       <div className="home-container">
         <h2 className="home-title">Добро пожаловать в <b>DevSphere Online IT Academy</b></h2>
@@ -10,7 +13,9 @@ const Home = () => {
           <h3>Войдите, чтобы получить доступ к урокам !</h3>
          
           
-          <button onClick={() => window.location.href = "/login"} className="home-login-btn">Войти</button>
+          <button onClick={() => navigate('/login')} className="home-login-btn">
+      Войти
+    </button>
         </div>
       </div>
     );
